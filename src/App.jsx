6ff68e20953da49e9fe33be84163689703e096a9
Jull1ano <iulian181@gmail.com>
2024-10-21 17:03:30 +0300

    blur

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Dashboard from './pages/Dashboard'
 import Create from './pages/Create'
 import Profile from './pages/Profile'
 import Pie from './pages/Pie'
+import Old from './pages/old'
 
 // router and routes
 const router = createBrowserRouter(
@@ -20,6 +21,7 @@ const router = createBrowserRouter(
       <Route path="create" element={<Create />} />
       <Route path="profile" element={<Profile />} />
       <Route path="pie" element={<Pie />} />
+      <Route path="old" element={<Old />} />
     </Route>
   )
 )
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,33 +1,24 @@
-import { Flex, Heading, SimpleGrid, VStack } from "@chakra-ui/react"
-import InvestmentPieChart from "./Pie"
+import { Container, Flex, Heading, SimpleGrid, VStack, Text, Box } from "@chakra-ui/react"
 
 export default function Dashboard() {
+
+const boxStyles = {
+ p: '10px',
+ bg: 'purple.400',
+ textColor: 'white',
+ m: '10px',
+ textAlign: 'center',
+//  filter: 'blur(3px)',
+}
+
   return (
-    <VStack spacing={4} align="stretch">
-      <SimpleGrid columns={2} spacing={10}>
-        <Flex 
-          bg='blue.500' 
-          height='80px' 
-          alignItems='center' 
-          justifyContent='center'
-        >
-          Dashboard
-        </Flex>
-        <Flex 
-          bg='blue.500' 
-          height='80px' 
-          alignItems='center' 
-          justifyContent='center'
-        >
-          Tabla
-        </Flex>
-      </SimpleGrid>
-      <Heading align='center'>
-        Dashboard
-      </Heading>
-      <Flex>
-        <InvestmentPieChart />
-      </Flex>
-    </VStack>
+    <Container as='section' maxWidth="container.xl">
+      <Heading>Dashboard</Heading>
+      <Text color="blue.500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+      </Text>
+
+      <Box sx={boxStyles}>Hello</Box>
+    </Container>
   )
 }
