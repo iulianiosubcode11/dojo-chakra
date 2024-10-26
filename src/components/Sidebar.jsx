3de8e4c2804b24756b3149c852ddca5fc7a631bb
Jull1ano <iulian181@gmail.com>
2024-10-26 12:08:01 +0300

    nice Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,38 +1,68 @@
-import { List, ListItem, ListIcon, Box } from "@chakra-ui/react"
+import { List, ListItem, ListIcon, Box, Flex } from "@chakra-ui/react"
 import { NavLink } from "react-router-dom"
 import { CalendarIcon, EditIcon, AtSignIcon } from "@chakra-ui/icons"
+import { useState } from "react"
 
 export default function Sidebar() {
-  
+  const [hoveredItem, setHoveredItem] = useState(null)
+
+  // Color variables
+  const activeColor = '#FFE100'
+  const inactiveColor = 'white'
+
+  const NavItem = ({ to, icon, children }) => {
+    return (
+      <ListItem>
+        <NavLink 
+          to={to} 
+          style={({ isActive }) => ({
+            color: isActive ? activeColor : inactiveColor,
+          })}
+        >
+          {({ isActive }) => (
+            <Box 
+              display='flex' 
+              alignItems='center'
+              color={isActive ? activeColor : (hoveredItem === to ? activeColor : inactiveColor)}
+              onMouseEnter={() => setHoveredItem(to)}
+              onMouseLeave={() => setHoveredItem(null)}
+              transition="color 0.2s"
+            >
+              <ListIcon 
+                as={icon} 
+                color={isActive || hoveredItem === to ? activeColor : inactiveColor}
+                transition="color 0.2s"
+              />
+              {children}
+            </Box>
+          )}
+        </NavLink>
+      </ListItem>
+    )
+  }
+
   return (
-    <List color='white' fontSize='1.2em' spacing={1}>
-      <ListItem>
-        <NavLink to='/'>
-          <Box display='flex' alignItems='center'>
-            <ListIcon as={CalendarIcon} />            
-            Dashboard
-          </Box>
-        </NavLink>
-      </ListItem>
-
-      <ListItem>
-        <NavLink to='/new-task'>
-          <Box display='flex' alignItems='center'>
-            <ListIcon as={EditIcon} />
-            New Task
-          </Box>
-        </NavLink>
-      </ListItem>
-
-      <ListItem>
-        <NavLink to='/profile'>
-          <Box display='flex' alignItems='center'>
-            <ListIcon as={AtSignIcon} />
-            Profile
-          </Box>
-        </NavLink>
-      </ListItem>
-    </List>
+    <Flex 
+      flexDirection={{ base: 'row', lg: 'column' }} 
+      justifyContent={{ base: 'center', lg: 'flex-start' }}
+      alignItems={{ base: 'center', lg: 'flex-start' }}
+      h={{ base: 'auto', lg: '100%' }}
+    >
+      <Flex
+        as={List}
+        color={inactiveColor}
+        fontSize='1.2em' 
+        gap={{ base: 6, lg: 3 }}
+        flexDirection={{ base: 'row', lg: 'column' }}
+        width="100%"
+        maxWidth={{ base: '450px', lg: '100%' }}
+        justifyContent={{ base: 'space-between', lg: 'flex-start' }}
+        alignItems={{ base: 'center', lg: 'flex-start' }}
+      >
+        <NavItem to="/" icon={CalendarIcon}>Dashboard</NavItem>
+        <NavItem to="/create" icon={EditIcon}>Create</NavItem>
+        <NavItem to="/profile" icon={AtSignIcon}>Profile</NavItem>
+      </Flex>
+    </Flex>
   )
 }
-
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,32 @@
+import { Box, FormControl, FormLabel, Input, FormHelperText } from "@chakra-ui/react"; 
+import { Form } from "react-router-dom"; 
+
+
 export default function Create() {
   return (
-    <div>Create</div>
+    <Box maxW='480px'>
+      <Form>
+        <FormControl isRequired mb='40px'>
+        <FormLabel color="gray.800">
+            Title
+            <Box as="span" color="tomato" ml={1}>
+              *
+            </Box>
+          </FormLabel>
+          <Input 
+            type="text" 
+            bg="white"
+            color="gray.800"
+            border="0px solid"
+            borderColor="gray.300"
+            _hover={{ borderColor: "gray.400" }}
+            _focus={{ borderColor: "purple.500", boxShadow: "0 0 0 1px purple.500" }}
+          />
+          <FormHelperText color="gray.500">
+            Enter a descriptive task name
+          </FormHelperText>
+        </FormControl>
+      </Form>
+    </Box>
   )
 }
