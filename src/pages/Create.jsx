8861ba62b4e65ab6e96187819c60f18ffe18c3e4
Jull1ano <iulian181@gmail.com>
2024-10-28 11:21:32 +0200

    good Monday

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,32 +1,103 @@
-import { Box, FormControl, FormLabel, Input, FormHelperText } from "@chakra-ui/react"; 
-import { Form } from "react-router-dom"; 
+import { Box, FormControl, FormLabel, Input, FormHelperText, Textarea, Checkbox, Button, Flex, Container } from "@chakra-ui/react";
+import { Form } from "react-router-dom";
 
 
 export default function Create() {
   return (
-    <Box maxW='480px'>
-      <Form>
-        <FormControl isRequired mb='40px'>
-        <FormLabel color="gray.800">
-            Title
-            <Box as="span" color="tomato" ml={1}>
-              *
-            </Box>
-          </FormLabel>
-          <Input 
-            type="text" 
-            bg="white"
-            color="gray.800"
-            border="0px solid"
-            borderColor="gray.300"
-            _hover={{ borderColor: "gray.400" }}
-            _focus={{ borderColor: "purple.500", boxShadow: "0 0 0 1px purple.500" }}
-          />
-          <FormHelperText color="gray.500">
-            Enter a descriptive task name
-          </FormHelperText>
-        </FormControl>
-      </Form>
-    </Box>
+    <Container 
+      display="flex"
+      justifyContent="flex-start" //align items to the left horizontally
+      alignItems="flex-start" //align items to the bottom vertically
+      p={0}        
+      minW="100%"    
+    >
+      <Box
+        w="100%"
+        maxW='480px'
+        mt={4}              
+        bg="white"
+        p={8}
+        borderRadius="lg"
+        boxShadow="sm"
+        border="1px solid"
+        borderColor="gray.200"
+      >
+        <Form>
+          <FormControl mb='40px'>
+            <FormLabel color="gray.800">
+              Title
+              <Box as="span" color="tomato" ml={1}>
+                *
+              </Box>
+            </FormLabel>
+            <Input
+              type="text"
+              bg="white"
+              color="gray.800"
+              border="1px solid"
+              borderColor="gray.300"
+              _hover={{ borderColor: "gray.400" }}
+              _focus={{ borderColor: "purple.500", boxShadow: "0 0 0 1px purple.500" }}
+              placeholder="Enter task title"
+              _placeholder={{ color: "gray.400" }}
+            />
+            <FormHelperText color="gray.500">
+              Enter a descriptive task name
+            </FormHelperText>
+          </FormControl>
+          <FormControl mb='40px' isRequired>
+            <FormLabel color="gray.800">Task description:</FormLabel>
+            <Textarea
+              bg="white"
+              color="gray.800"
+              border="1px solid"
+              borderColor="gray.300"
+              _hover={{ borderColor: "gray.400" }}
+              _focus={{ borderColor: "purple.500", boxShadow: "0 0 0 1px purple.500" }}
+              placeholder="Enter a description for your task"
+              _placeholder={{ color: "gray.400" }}
+              name="description"
+            />
+          </FormControl>
+          <FormControl>
+            <Checkbox
+              name="isPriority"
+              colorScheme="purple"
+              size='lg'
+              // defaultChecked
+              sx={{
+                'span.chakra-checkbox__control': {
+                  bg: 'white',
+                  borderColor: 'gray.300',
+                  borderWidth: '1px',  // Set border width to 1px
+                  borderRadius: '4px',
+                  _hover: { borderColor: 'gray.400' },
+                  _focus: { borderColor: 'purple.500', boxShadow: '0 0 0 1px purple.500' },
+                }
+              }}
+            >
+              <FormLabel color="gray.800" mb={0} ml={1}>Set as priority task</FormLabel>
+            </Checkbox>
+          </FormControl>
+          <Flex justifyContent="space-between" mt='32px' gap={4}>
+            <Button type="submit" colorScheme="purple" bg="purple.200" size="lg" w='full'
+              _hover={{ color: "gray.800", bg: "purple.300" }}
+            >Create Task</Button>
+            <Button
+              type="reset"
+              color="gray.600"
+              border="2px solid"
+              borderColor="gray.400"
+              bg="white"
+              _hover={{ borderColor: "gray.400", color: "gray.800", bg: "gray.100" }}
+              size="lg"
+              w='full'
+            >
+              Cancel
+            </Button>
+          </Flex>
+        </Form>
+      </Box>
+    </Container>
   )
 }
